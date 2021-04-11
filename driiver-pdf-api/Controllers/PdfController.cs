9debda3e7ed4c870e3e32ff74b219dea926cde87
thomaskleist <thomas@kleist-it.com>
2021-04-11 09:07:35 +0200

    added pdfcontroller

---
+++ driiver-pdf-api/Controllers/PdfController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace driiver_pdf_api.Controllers
+{
+    [ApiController]
+    [Route("pdf")]
+    public class PdfController: ControllerBase
+    {
+        public PdfController()
+        {
+        }
+
+        [HttpGet]
+        [Route("generate/{json}")]
+        public Task<string> GeneratePDF(string json)
+        {
+            try
+            {
+                return Task.FromResult(json)
+            } catch(Exception ex)
+            {
+                throw new Exception(ex.HelpLink, ex.InnerException);
+            }
+        }
+    }
+}
