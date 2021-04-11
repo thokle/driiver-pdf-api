673f985de5c359e426a8b08fedddc45c78f1ccd9
thomaskleist <thomas@kleist-it.com>
2021-04-11 08:48:40 +0200

    reset url

--- driiver-pdf-api/Startup.cs
+++ driiver-pdf-api/Startup.cs
@@ -41,7 +41,11 @@ namespace driiver_pdf_api
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "driiver_pdf_api v1"));
+                app.UseSwaggerUI(c => {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "driiver_pdf_api v1");
+                    c.RoutePrefix = string.Empty;
+                } );
+                
             }
 
             app.UseHttpsRedirection();
